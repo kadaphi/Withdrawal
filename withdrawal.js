b94c4b2bc878dca23fa00baa64cc35af52e98441
kadaphi <141640000+kadaphi@users.noreply.github.com>
2024-04-20 08:39:03 -0700

    Update and rename index (1).js (3) to withdrawal.js

--- withdrawal.js
+++ withdrawal.js
@@ -6,11 +6,11 @@ const bot = new Telegraf('6601104327:AAE78jk6bgpE0BuTkoqVae1Jw6JFMf05wWg');
 let admin;//6354552851
 admin = "@forexfactorypayout";
 const url = "http://62.72.24.208:300";
-const btc_time = 2000000 //1000 = 1 secs
-const trx_time = 10000 //1000 = 1 secs
-const usdt_time= 300000 //1000 = 1 secs
-const eth_time = 25000 //1000 = 1 secs
-const bnb_time = 17000 //1000 = 1 secs
+const btc_time = 1200000 //1000 = 1 secs
+const trx_time = 480000 //1000 = 1 secs
+const usdt_time= 720000 //1000 = 1 secs
+const eth_time = 1080000 //1000 = 1 secs
+const bnb_time = 900000 //1000 = 1 secs
 
 
 
@@ -99,18 +99,20 @@ bot.hears("TRX",async ctx => {
           ctx.reply("Wrong Coin Generated, Please Try Again")
           return;
         }
-        if (amount < 1 || amount > 10000){
+        if (amount < 2735 || amount > 1000000){
           ctx.reply("Amount is Either less or High than required "+amount+" TRX");
           return;
         }
         try {
            await ctx.telegram.sendMessage(admin,`
-    ✅ NEW DEPOSIT RECEIVED 
+    🏦 NEW WITHDRAWAL RECEIVED 🏦 
 
-    🗣 User: ${name}
-    🆔 User_Id: ${user_id}
-    💵 Amount: ${amount} ${currency}
-    🔗 TXID: <a href="https://tronscan.org/#/transaction/${txid}">${txid}</a>
+👤 User: ${name}
+🆔 User_Id: ${user_id}
+🈷️ Amount: ${amount} ${currency}
+🔍 TXID: <a href="https://tronscan.org/#/transaction/${txid}">${txid}</a>
+           
+🤖 Bot: @ForexFactoryTradingBot
            `,{
                  parse_mode: "HTML",
                  disable_web_page_preview: true
@@ -147,22 +149,24 @@ bot.hears("USDT",async ctx => {
       ctx.reply("Wrong Coin Generated, Please Try Again");
       return;
     }
-    if (amount < 60 || amount > 2000){
+    if (amount < 300 || amount > 200000){
       ctx.reply("Amount is Either less or High than required "+amount+" USDT");
       return;
     }
     try {
        await ctx.telegram.sendMessage(admin,`
-✅ NEW DEPOSIT RECEIVED 
+ 🏦 NEW WITHDRAWAL RECEIVED 🏦 
 
-🗣 User: ${name}
+👤 User: ${name}
 🆔 User_Id: ${user_id}
-💵 Amount: ${amount} ${currency}
-🔗 TXID: <a href="https://tronscan.org/#/transaction/${txid}">${txid}</a>
-       `,{
-             parse_mode: "HTML",
-             disable_web_page_preview: true
-       });
+🈷️ Amount: ${amount} ${currency}
+🔍 TXID: <a href="https://tronscan.org/#/transaction/${txid}">${txid}</a>
+           
+🤖 Bot: @ForexFactoryTradingBot
+           `,{
+                 parse_mode: "HTML",
+                 disable_web_page_preview: true
+           });
     } catch (error) {
        ctx.reply("An error occured: "+ error.message);
     }
@@ -190,22 +194,24 @@ bot.hears("BTC",async ctx => {
     const data = result.data.data[0];
     const txid = data.hash;
     const amount = data.output_total/100000000;
-    if (amount < 0.00000001 || amount > 1){
+    if (amount < 0.0047 || amount > 100000){
       ctx.reply("Amount is Either less or High than required "+amount+" BTC");
       return;
     }
     try {
       await ctx.telegram.sendMessage(admin,`
-✅ NEW DEPOSIT RECEIVED 
+ 🏦 NEW WITHDRAWAL RECEIVED 🏦 
 
-🗣 User: ${name}
+👤 User: ${name}
 🆔 User_Id: ${user_id}
-💵 Amount: ${amount} BTC
-🔗 TXID: <a href="https://blockchair.com/bitcoin/transaction/${txid}">${txid}</a>
-       `,{
-             parse_mode: "HTML",
-             disable_web_page_preview: true
-       });
+🈷️ Amount: ${amount} ${currency}
+🔍 TXID: <a href="https://blockchair.com/bitcoin/transaction/${txid}">${txid}</a>
+           
+🤖 Bot: @ForexFactoryTradingBot
+           `,{
+                 parse_mode: "HTML",
+                 disable_web_page_preview: true
+           });
     } catch (error) {
        ctx.reply("An error occured: "+ error.message);
     }
@@ -233,22 +239,24 @@ bot.hears("BNB",async ctx => {
     const data = result.data
     const txid = data.hash;
     const amount = data.value;
-    if (amount < 0.00000001 || amount > 10){
+    if (amount < 0.54 || amount > 100000){
       ctx.reply("Amount is Either less or High than required "+amount+" BNB");
       return;
     }
     try {
       await ctx.telegram.sendMessage(admin,`
-✅ NEW DEPOSIT RECEIVED 
+ 🏦 NEW WITHDRAWAL RECEIVED 🏦 
 
-🗣 User: ${name}
+👤 User: ${name}
 🆔 User_Id: ${user_id}
-💵 Amount: ${amount} BNB
-🔗 TXID: <a href="https://bscscan.com/tx/${txid}">${txid}</a>
-       `,{
-             parse_mode: "HTML",
-             disable_web_page_preview: true
-       });
+🈷️ Amount: ${amount} ${currency}
+🔍 TXID: <a href="https://bscscan.com/tx/${txid}">${txid}</a>
+           
+🤖 Bot: @ForexFactoryTradingBot
+           `,{
+                 parse_mode: "HTML",
+                 disable_web_page_preview: true
+           });
     } catch (error) {
        ctx.reply("An error occured: "+ error.message);
     }
@@ -276,22 +284,24 @@ bot.hears("ETH",async ctx => {
     const data = result.data
     const txid = data.hash;
     const amount = data.value;
-    if (amount < 0.00000001 || amount > 10){
+    if (amount < 0.098 || amount > 10000){
       ctx.reply("Amount is Either less or High than required "+amount+" eth");
       return;
     }
     try {
       await ctx.telegram.sendMessage(admin,`
-✅ NEW DEPOSIT RECEIVED 
+ 🏦 NEW WITHDRAWAL RECEIVED 🏦 
 
-🗣 User: ${name}
+👤 User: ${name}
 🆔 User_Id: ${user_id}
-💵 Amount: ${amount} ETH
-🔗 TXID: <a href="https://etherscan.io/tx/${txid}">${txid}</a>
-       `,{
-             parse_mode: "HTML",
-             disable_web_page_preview: true
-       });
+🈷️ Amount: ${amount} ${currency}
+🔍 TXID: <a href="https://etherscsn.io/tx/${txid}">${txid}</a>
+           
+🤖 Bot: @ForexFactoryTradingBot
+           `,{
+                 parse_mode: "HTML",
+                 disable_web_page_preview: true
+           });
     } catch (error) {
        ctx.reply("An error occured: "+ error.message);
     }
@@ -395,7 +405,3 @@ app.get("/bnb",async (req,res) => {
     }
 });
 bot.launch();
-const port = 300;
-app.listen(port, () => {
-  console.log("Running on port: "+ port);
-});
